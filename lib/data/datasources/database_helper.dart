a39e1edfbaf5e04742715943a6a426430a5f81fc
NCS Developer <developer@ncs.local>
2026-08-27 01:18:32 +0900

    fix: revert internal type mapping and bump DB v26

--- lib/data/datasources/database_helper.dart
+++ lib/data/datasources/database_helper.dart
@@ -14,7 +14,7 @@ class DatabaseHelper {
 
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDB('vocational_counselor2_v25.db');
+    _database = await _initDB('vocational_counselor2_v26.db');
     return _database!;
   }
 
